0a2e12a5e6d7b8a241da585cbcaca8ce0433ddba
Daniel Sloof <daniel@rubic.nl>
2026-02-07 23:17:40 +0100

    eliminate yara dependency in go test

--- cmd/internal/compile.go
+++ cmd/internal/compile.go
@@ -1,31 +1,9 @@
 package internal
 
 import (
-	"os"
-
-	yara "github.com/hillu/go-yara/v4"
 	"github.com/sansecio/yargo/parser"
 	"github.com/sansecio/yargo/scanner"
 )
-
-func GoYaraRules(yaraFile string) (*yara.Rules, error) {
-	compiler, err := yara.NewCompiler()
-	if err != nil {
-		return nil, err
-	}
-
-	f, err := os.Open(yaraFile)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-
-	if err := compiler.AddFile(f, ""); err != nil {
-		return nil, err
-	}
-
-	return compiler.GetRules()
-}
 
 func YargoRules(yaraFile string) (*scanner.Rules, error) {
 	p := parser.New()
--- cmd/sort-diff/main.go
+++ cmd/sort-diff/main.go
@@ -1,3 +1,5 @@
+//go:build yara
+
 package main
 
 import (
